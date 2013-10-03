a48c255524b6ad80754c6a8578358453d5f34abe
Danil Fomichev <fomichev@ngs.ru>
2013-10-04 01:45:20 +0200

    get_tree method for categories class

--- myshop/app/controllers/categories_controller.rb
+++ myshop/app/controllers/categories_controller.rb
@@ -7,4 +7,20 @@ class CategoriesController < ApplicationController
 
   def show
   end
+  private
+  def get_tree(obj=self,cid="null",h=Array.new)
+        obj.each do |a|
+                if a.kind_of?(Hash)
+                is_deleted=a["attr"].has_key?("is_deleted")?"true":"false"
+                is_new=a["attr"].has_key?("is_new")?"true":"false"
+                h.push( {:id=>a["attr"]["_cid"],:name=>a["data"],:pid=>cid,:delete=>is_deleted,:new=>is_new})
+                 if a.has_key?("children")
+                        c=get_tree(a["children"],a["attr"]["_cid"])
+                        h.concat(c)
+                 end
+                end
+        end
+  	return h
+  end
+
 end
