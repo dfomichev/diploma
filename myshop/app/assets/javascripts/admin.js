8cd9cdd30312816f9f73eede789435618bb56fe9
Danil Fomichev <fomichev@ngs.ru>
2013-10-22 06:55:24 +0200

    Multiple fixes

--- myshop/app/assets/javascripts/admin.js
+++ myshop/app/assets/javascripts/admin.js
@@ -1,11 +1,18 @@
-(function($) {
-        $(function() {
+$('ul.tabs').on('click', 'li:not(.current)', function() {
+     $(this).addClass('current').siblings().removeClass('current')
+           .parents('div.section').find('div.box').eq($(this).index()).show().siblings('div.box').hide();
+     var href=$(this).attr('href'); 
+     if (typeof href!=="undefined"){
+         var box=$(this).parents('div.section').find('div.box').eq($(this).index());
+         $.ajax({
+                           type: "GET",
+                            url: href,
+                            success: function(data){
+                                $(box).empty().html(data);
+                            }
+                        });
 
-          $('ul.tabs').on('click', 'li:not(.current)', function() {
-               $(this).addClass('current').siblings().removeClass('current')
-               .parents('div.section').find('div.box').eq($(this).index()).fadeIn(150).siblings('div.box').hide();
-              
-          })
+    }
+    return false;
+});
           
-        })
-    })(jQuery)
--- myshop/app/assets/javascripts/common.js
+++ myshop/app/assets/javascripts/common.js
@@ -32,75 +32,7 @@ $(document).ready(function() {
             return false;
 
      });  
-  $(function () {
 
-        $("#tree").click(function () {
-          if ( $("#tree").jstree("get_selected").attr("blocked" )){
-                $("#delNode").attr('disabled', 'disabled');
-          }else{
-                $("#delNode").removeAttr('disabled');
-          }
-        });
-
-        $("#addCNode").click(function () {
-                $("#tree").jstree("create",null,false,{ attr: { id : rand(16),is_new: "true" }, data: "Some other child node" });
-        });
-
-        $("#addPNode").click(function () {
-                $("#tree").jstree("create",-1,"first",{ attr: { id : rand(16) ,is_new: "true"}, data: "Some other 1-st level node" });
-        });
-
-        $("#renNode").click(function () {
-                $("#tree").jstree("rename");
-        });
-
-        $("#delNode").click(function () {
-           $("#tree").jstree("get_selected").hide();
-           $("#tree").jstree("get_selected").attr("is_deleted","true");
-           $("#tree").jstree('select_node',$("#tree").find('li')[0]);
-        });
-
-        $("#saveTree").click(function () {
-                _json=$("#tree").jstree("get_json",-1,["is_deleted","id","is_new"]);
-                $.post( "/categories/save",{ json : _json}).done(function( data ) {
-                            $("#json").append(data);
-                });
-
-        });
-	$("#tree").jstree({
-		"json_data" : {
-			"ajax" : {"url" : "/categories/json"}
-		},
-                "dnd" : {
-                        "drop_finish" : function () {
-                        },
-                        "drag_check" : function (data) {
-                                if(data.r.attr("id") == "phtml_1") {
-                                        return false;
-                                }
-                                return {
-                                        after : false,
-                                        before : false,
-                                        inside : true
-                                };
-                        },
-                        "drag_finish" : function (data) {
-                        }
-                },
-                "ui" : {
-                        "select_limit" : 1
-                },
-                "checkbox" :{
-                    "two_state": true,
-                    "real_checkboxes": true,
-                    "real_checkboxes_names": function (n) { return [ ("cat["+ n[0].id )+"]",n[0].id ]}
-                },
-                "plugins" : [ "themes", "ui", "crrm","dnd","json_data" ]
-        }).bind("select_node.jstree", function (NODE, REF_NODE) {
-            var a = $.jstree._focused().get_selected();
-    	});
-
-    });
   
 });
 
