640afca12a03116e8eb4e8d1c783865e09d328dd
Danil Fomichev <fomichev@ngs.ru>
2013-10-25 07:37:06 +0200

    add categories show

--- myshop/app/controllers/front_controller.rb
+++ myshop/app/controllers/front_controller.rb
@@ -1,5 +1,8 @@
 class FrontController < ApplicationController
-
+ def index
+  @categories=cat_tree(Categories.where(pid: "null")) ;
+#  render text: @categories   
+ end   
  def show
  if params.has_key?('id')
      @products=Products.find(params['id'])
@@ -13,5 +16,15 @@ class FrontController < ApplicationController
      render text: 'Error'
  end
  end
+
+  def cat_tree(obj=self,h={},a=Array.new)
+    obj.each do |v|
+        children=cat_tree(Categories.where(pid: v.id))
+        h={:name=>v.name,:id=>v.id,:children=>children}
+        a<<h
+    end
+      return a
+  end
+
 
 end
