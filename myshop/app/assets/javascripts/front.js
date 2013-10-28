8d086627ceac6d28d856de504a8e49193f3bcb78
Danil Fomichev <fomichev@ngs.ru>
2013-10-28 05:28:20 +0100

    add products  show

--- myshop/app/assets/javascripts/front.js
+++ myshop/app/assets/javascripts/front.js
@@ -1,1 +1,35 @@
- 
+
+
+    var listProducts = function(obj) {
+      var cid=$(obj).attr('id')        
+      if ( $(obj).attr('class')=='cc'){
+      $.ajax({
+                           type: "GET",
+                            url: "/front/"+cid+"/children",
+                            success: function(data){
+                                $("#cContainer").empty().html(data);
+                            }
+                        });
+      }
+      $.ajax({
+                           type: "GET",
+                            url: "/front/"+cid+"/products",
+                            success: function(data){
+                                $("#pContainer").empty().html(data);
+                            }
+                        });
+        return false;
+
+     };
+   var showProduct = function(obj) {
+      var cid=$(obj).attr('id')        
+      $.ajax({
+                           type: "GET",
+                            url: "/front/"+cid+"/show",
+                            success: function(data){
+                                $("div.pDetails").empty().html(data);
+                            }
+                        });
+            return false;
+
+     };
